54edced6d9c2b7f2ceb42cf808f2f94a498afdb2
Hoang Tuan Anh <tuananh45468@gmail.com>
2022-05-24 14:36:06 +0700

    change app to main

---
+++ seeds/creditCard.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        number: 111111,
+        expireDate: '10/10/2022',
+        cvv: 411
+    },
+
+    {
+        number: 222222,
+        expireDate: '11/11/2022',
+        cvv: 443
+    },
+
+    {
+        number: 333333,
+        expireDate: '12/12/2022',
+        cvv: 577
+    }
+]
+
+//console.log(creditCard[0].expireDate);
---
+++ seeds/mobileCard.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        name: 'Viettel',
+        number: 111111
+    },
+
+    {
+        name: 'Mobifone',
+        number: 222222
+    },
+
+    {
+        name: 'Vinaphone',
+        number: 333333
+    },
+
+]
